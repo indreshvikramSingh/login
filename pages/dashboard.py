b12181c3c28b51a6c2130e08918cc6745e780768
Pratyaksh <prtksh510@gmail.com>
2025-04-18 13:05:32 +0530

    Added code to fetch from server

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -110,9 +110,10 @@ from plotly.subplots import make_subplots
 import plotly.graph_objs as go
 import requests
 import psutil
-from io import StringIO
+# from io import StringIO
 
-# ---------- Helper Function ----------
+st.spinner()
+
 def find_sd_card_drive():
     partitions = psutil.disk_partitions()
     for p in partitions:
@@ -150,43 +151,60 @@ if not st.session_state.import_clicked:
     st.stop()
 
 # ---------- Step 2: Choose Source ----------
-data_source = st.radio("Choose Data Source:", ["Upload CSV", "SD Card"])
+data_source = st.radio("Choose Data Source:", ["Fetch from server", "Fetch from SD Card"])
 df = None
 
-
-
 # ---------- Upload CSV ----------
-if data_source == "Upload CSV":
-    uploaded_file = st.file_uploader("Upload CSV file", type=["csv"])
-    if uploaded_file is not None:
-        df = pd.read_csv(uploaded_file, header=None)
-        st.success(" CSV uploaded successfully!")
-
-
-# ---------- SD Card Upload ----------
-elif data_source == "SD Card":
-    sd_drive = find_sd_card_drive()
+if data_source == "Fetch from server":
     
     st.title("Fetch CSV from Remote API")
 
-    api_url = "http://f595kzbv4gnter-md8lqmsh.streamlit.app"
+    api_url = 'https://deckmount.in/api/web/indresh.php?user_id=1'
 
     if st.button("Fetch CSV"):
         try:
             response = requests.get(api_url)
 
             if response.status_code == 200:
-                csv_content = StringIO(response.text)
-                df = pd.read_csv(csv_content)
+
+                data = response.json()
+                record = data.get('data', {}).get('datafile', [])
+                file_info = record[0]
+
+                file_name = file_info.get('file_name')
+                file_path = file_info.get('file_path')
+                print(f"File Name: {file_name}")
+                print(f"File Path: {file_path}")
+
+                file_url = f"{file_path}/{file_name}"
+                print(f"File URL: {file_url}")
+
+                csv_response = requests.get(file_url)
+                print("csv Response", csv_response.text)
+
+                with open(file_name, 'wb') as f:
+                    f.write(csv_response.content)
+
+                with open(file_name, 'r', encoding='utf-8') as csvfile:
+                    reader = csv.reader(csvfile)
+                    for row in reader:
+                        print(row)
+
+                # csv_content = StringIO(csv_response.text)
+                # df = pd.read_csv(csv_content)
 
                 st.success("CSV fetched successfully!")
-                st.dataframe(df)
+                # st.dataframe(df)
 
             else:
                 st.error(f"Failed to fetch CSV. Status code: {response.status_code}")
 
         except requests.exceptions.RequestException as e:
             st.error(f"Request error: {e}")
+
+# ---------- SD Card Upload ----------
+elif data_source == "Fetch from SD Card":
+    sd_drive = find_sd_card_drive()
     
     if sd_drive:
         st.success(f" SD card detected: {sd_drive}")
